16204ab5b98dc45d13524959d01536628df9dcfa
namjy007 <namjy007@mymail.unisa.edu.au>
2023-11-15 23:31:14 +1030

    Added functionality to display the initial properties of the created potion.

--- MAINCODE.py
+++ MAINCODE.py
@@ -3,7 +3,7 @@ class Reagent:
         self.name = name
         self.potency = potency
         self.quality = quality
-
+    
     def __str__(self):
         if self.quality is not None:
             return f'{self.name}: Potency {self.potency}, Quality {self.quality}'
